445d217f74acd82eb3bccb4786c64dea6f758743
TANHEHANZ <hantach10@gmail.com0>
2023-11-29 19:23:43 -0400

    redirect: children data views

--- app/SkillSwap/src/app/login/index.js
+++ app/SkillSwap/src/app/login/index.js
@@ -1,12 +1,16 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { router } from "expo-router";
 
 const Login = () => {
+  const redireccionar = () => {
+    router.replace("/views");
+  };
   return (
     <View>
       <Text>Login</Text>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => redireccionar()}>
         <Text>iniciar</Text>
       </TouchableOpacity>
     </View>
--- app/SkillSwap/src/app/views/_layout.js
+++ app/SkillSwap/src/app/views/_layout.js
@@ -1,13 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
 import NavBottom from "../../components/navBottom";
+import { Slot } from "expo-router";
 
 const HomeLayaut = () => {
   return (
     <View>
       <Text>HomeLayaut</Text>
       <View>
-        <Solt />
+        <Slot />
       </View>
       <NavBottom />
     </View>
